64a2682b19137329231b71136a5ccedef5cc5ea4
tremoloplusdelay <aachavda0789@gmail.com>
2022-03-18 16:35:39 +0530

    page view schema

--- spark_streaming/schema.py
+++ spark_streaming/schema.py
@@ -1,3 +1,4 @@
+from socket import inet_ntoa
 from pyspark.sql.types import IntegerType, StringType, DecimalType, TimestampType, StructField, StructType, LongType
 
 listen_events_schema = StructType([
@@ -20,4 +21,29 @@ listen_events_schema = StructType([
     StructField("firstName", StringType(), True),
     StructField("gender", StringType(), True),
     StructField("registration", LongType(), True)
+])
+
+page_view_events_schema = StructType([
+    StructField("ts", LongType(), True),
+    StructField("sessionId", IntegerType(), True),
+    StructField("page", StringType(), True),
+    StructField("auth", StringType(), True),
+    StructField("method", StringType(), True),
+    StructField("status", IntegerType(), True),
+    StructField("level", StringType(), True),
+    StructField("itemInSession", IntegerType(), True),
+    StructField("city", StringType(), True),
+    StructField("zip", IntegerType(), True),
+    StructField("state", StringType(), True),
+    StructField("userAgent", StringType(), True),
+    StructField("lon", DecimalType(), True),
+    StructField("lat", DecimalType(), True),
+    StructField("userId", IntegerType(), True),
+    StructField("lastName", StringType(), True),
+    StructField("firstName", StringType(), True),
+    StructField("gender", StringType(), True),
+    StructField("registration", LongType(), True),
+    StructField("artist", StringType(), True),
+    StructField("song", StringType(), True),
+    StructField("duration", DecimalType(), True)
 ])
