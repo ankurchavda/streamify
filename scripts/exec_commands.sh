02cf2032a13db4c1dc16e49812c2153e43b957d2
tremoloplusdelay <aachavda0789@gmail.com>
2022-03-18 16:34:35 +0530

    spark streaming commands

--- scripts/exec_commands.sh
+++ scripts/exec_commands.sh
@@ -4,6 +4,19 @@ docker run -it \
   events:1.0 \
     -c "examples/example-config.json" \
     --start-time "`date +"%Y-%m-%dT%H:%M:%S"`" \
-    --end-time "2022-03-18T17:00:00" --nusers 20000 \
+    --end-time "2022-03-18T17:00:00" --nusers 200000 \
     --kafkaBrokerList localhost:9092 \
+    --noverbose \
     --continuous
+
+#Stream page view events into Spark
+spark-submit \
+  --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.3 \
+  --deploy-mode cluster \
+stream_page_view_events.py
+
+#Stream listen events into Spark
+spark-submit \
+  --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.3 \
+  --deploy-mode cluster \
+stream_listen_events.py
