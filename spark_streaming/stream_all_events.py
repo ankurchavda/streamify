75fb10d9bc7784d6a856b5f17d05ce588417bfb2
tremoloplusdelay <aachavda0789@gmail.com>
2022-03-19 14:19:55 +0530

    stream all events from one

--- spark_streaming/stream_all_events.py
+++ spark_streaming/stream_all_events.py
@@ -1,102 +1,51 @@
 # Run the script using the following command
 # spark-submit \
 #   --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.3 \
-#   --deploy-mode cluster \
 # stream_all_events.py
 
 import os
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col, month, hour, dayofmonth, col, year
-from schema import listen_events_schema, page_view_events_schema
+from streaming_functions import *
+from schema import listen_events_schema, page_view_events_schema, auth_events_schema
 
 KAFKA_ADDRESS = os.getenv("KAFKA_ADDRESS", 'localhost')
-
+KAFKA_PORT = "9092"
 # will raise error if env variable not found
 GCS_STORAGE_PATH = os.environ["GCS_STORAGE_PATH"]
+LISTEN_EVENTS_TOPIC = "listen_events"
+PAGE_VIEW_EVENTS_TOPIC = "page_view_events"
+AUTH_EVENTS_TOPIC = "auth_events"
 
 # initialize a spark session
-spark = (SparkSession
-         .builder
-         .appName("All Events")
-         .master("yarn")
-         .getOrCreate())
+spark = create_or_get_spark_session('Eventsim Stream')
 
 # listen events stream
-listen_events = (spark
-                 .readStream
-                 .format("kafka")
-                 .option("kafka.bootstrap.servers", f"{KAFKA_ADDRESS}:9092")
-                 .option("startingOffsets", "earliest")
-                 .option("subscribe", "listen_events")
-                 .load())
-
-# read only value from the incoming message and convert the contents
-# inside to the passed schema
-listen_events = (listen_events
-                .selectExpr("CAST(value AS STRING)")
-                .select(
-                    from_json(col("value"), listen_events_schema).alias("data")
-                    )
-                .select("data.*")
-                )
-
-# Add month, day, hour to split the data into separate directories
-listen_events = (listen_events
-                 .withColumn("ts", (col("ts")/1000).cast("timestamp"))
-                 .withColumn("year", year(col("ts")))
-                 .withColumn("month", month(col("ts")))
-                 .withColumn("hour", hour(col("ts")))
-                 .withColumn("day", dayofmonth(col("ts")))
-                 )
-
+listen_events = create_kafka_read_stream(
+    spark, KAFKA_ADDRESS, KAFKA_PORT, LISTEN_EVENTS_TOPIC)
+listen_events = process_stream(listen_events, listen_events_schema)
 
 # page view stream
-page_view_events = (spark
-                 .readStream
-                 .format("kafka")
-                 .option("kafka.bootstrap.servers", f"{KAFKA_ADDRESS}:9092")
-                 .option("startingOffsets", "earliest")
-                 .option("subscribe", "page_view_events")
-                 .load())
+page_view_events = create_kafka_read_stream(
+    spark, KAFKA_ADDRESS, KAFKA_PORT, PAGE_VIEW_EVENTS_TOPIC)
+page_view_events = process_stream(page_view_events, page_view_events_schema)
 
-# read only value from the incoming message and convert the contents
-# inside to the passed schema
-page_view_events = (page_view_events
-                .selectExpr("CAST(value AS STRING)")
-                .select(
-                    from_json(col("value"), page_view_events_schema).alias("data")
-                    )
-                .select("data.*")
-                )
-
-# Add month, day, hour to split the data into separate directories
-page_view_events = (page_view_events
-                 .withColumn("ts", (col("ts")/1000).cast("timestamp"))
-                 .withColumn("year", year(col("ts")))
-                 .withColumn("month", month(col("ts")))
-                 .withColumn("hour", hour(col("ts")))
-                 .withColumn("day", dayofmonth(col("ts")))
-                 )
-
+# auth stream
+auth_events = create_kafka_read_stream(
+    spark, KAFKA_ADDRESS, KAFKA_PORT, AUTH_EVENTS_TOPIC)
+auth_events = process_stream(auth_events, auth_events_schema)
 
 # write a file to storage every 2 minutes in parquet format
-(listen_events
-    .writeStream
-    .format("parquet")
-    .partitionBy("month", "day", "hour")
-    .option("path", f"{GCS_STORAGE_PATH}/listen_events/")
-    .option("checkpointLocation", f"{GCS_STORAGE_PATH}/cp_listen_events/")
-    .trigger(processingTime='120 seconds')
-    .outputMode("append")
-    .start())
+listen_events_writer = create_file_write_stream(listen_events,
+                                                f"{GCS_STORAGE_PATH}/{LISTEN_EVENTS_TOPIC}",
+                                                f"{GCS_STORAGE_PATH}/checkpoint/{LISTEN_EVENTS_TOPIC}")
 
-(page_view_events
-    .writeStream
-    .format("parquet")
-    .partitionBy("month", "day", "hour")
-    .option("path", f"{GCS_STORAGE_PATH}/page_view_events/")
-    .option("checkpointLocation", f"{GCS_STORAGE_PATH}/cp_page_view_events/")
-    .trigger(processingTime='120 seconds')
-    .outputMode("append")
-    .start()
-    .awaitTermination())
+page_view_events_writer = create_file_write_stream(page_view_events,
+                                                   f"{GCS_STORAGE_PATH}/{PAGE_VIEW_EVENTS_TOPIC}",
+                                                   f"{GCS_STORAGE_PATH}/checkpoint/{PAGE_VIEW_EVENTS_TOPIC}")
+
+auth_events_writer = create_file_write_stream(auth_events,
+                                              f"{GCS_STORAGE_PATH}/{AUTH_EVENTS_TOPIC}",
+                                              f"{GCS_STORAGE_PATH}/checkpoint/{AUTH_EVENTS_TOPIC}")
+
+listen_events_writer.start()
+page_view_events_writer.start()
+auth_events_writer.start().awaitTermination()
---
+++ spark_streaming/streaming_functions.py
@@ -0,0 +1,115 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json, col, month, hour, dayofmonth, col, year
+
+
+def create_or_get_spark_session(app_name, master="yarn"):
+    """
+    Creates or gets a Spark Session
+
+    Parameters:
+        app_name : str
+            Pass the name of your app
+        master : str
+            Choosing the Spark master, yarn is the default
+    Returns:
+        spark: SparkSession
+    """
+    spark = (SparkSession
+             .builder
+             .appName(app_name)
+             .master(master=master)
+             .getOrCreate())
+
+    return spark
+
+
+def create_kafka_read_stream(spark, kafka_address, kafka_port, topic, starting_offset="earliest"):
+    """
+    Creates a kafka read stream
+
+    Parameters:
+        spark : SparkSession
+            A SparkSession object
+        kafka_address: str
+            Host address of the kafka bootstrap server
+        topic : str
+            Name of the kafka topic
+        starting_offset: str
+            Starting offset configuration, "earliest" by default 
+    Returns:
+        read_stream: DataStreamReader
+    """
+
+    read_stream = (spark
+                   .readStream
+                   .format("kafka")
+                   .option("kafka.bootstrap.servers", f"{kafka_address}:{kafka_port}")
+                   .option("startingOffsets", starting_offset)
+                   .option("subscribe", topic)
+                   .load())
+
+    return read_stream
+
+
+def process_stream(stream, stream_schema):
+    """
+    Process stream to fetch on value from the kafka message.
+    convert ts to timestamp format and produce year, month, day,
+    hour columns
+    Parameters:
+        stream : DataStreamReader
+            The data stream reader for your stream
+    Returns:
+        stream: DataStreamReader
+    """
+
+    # read only value from the incoming message and convert the contents
+    # inside to the passed schema
+    stream = (stream
+              .selectExpr("CAST(value AS STRING)")
+              .select(
+                  from_json(col("value"), stream_schema).alias(
+                      "data")
+              )
+              .select("data.*")
+              )
+
+    # Add month, day, hour to split the data into separate directories
+    stream = (stream
+              .withColumn("ts", (col("ts")/1000).cast("timestamp"))
+              .withColumn("year", year(col("ts")))
+              .withColumn("month", month(col("ts")))
+              .withColumn("hour", hour(col("ts")))
+              .withColumn("day", dayofmonth(col("ts")))
+              )
+
+    return stream
+
+
+def create_file_write_stream(stream, storage_path, checkpoint_path, trigger="120 seconds", output_mode="append", file_format="parquet"):
+    """
+    Write the stream back to a file store
+
+    Parameters:
+        stream : DataStreamReader
+            The data stream reader for your stream
+        file_format : str
+            parquet, csv, orc etc
+        storage_path : str
+            The file output path
+        checkpoint_path : str
+            The checkpoint location for spark
+        trigger : str
+            The trigger interval
+        output_mode : str
+            append, complete, update
+    """
+
+    (stream
+    .writeStream
+    .format(file_format)
+    .partitionBy("month", "day", "hour")
+    .option("path", storage_path)
+    .option("checkpointLocation", checkpoint_path)
+    .trigger(processingTime=trigger)
+    .outputMode(output_mode))
