26355b5ada22537bd45c946f9fa9b08960e0a9d9
tremoloplusdelay <aachavda0789@gmail.com>
2022-03-22 16:08:37 +0530

    dataset setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -102,3 +102,17 @@ resource "google_dataproc_cluster" "mulitnode_spark_cluster" {
   }
 
 }
+
+resource "google_bigquery_dataset" "dev_dataset" {
+  dataset_id                 = var.dev_bq_dataset
+  project                    = var.project
+  location                   = var.region
+  delete_contents_on_destroy = true
+}
+
+resource "google_bigquery_dataset" "prod_dataset" {
+  dataset_id                 = var.prod_bq_dataset
+  project                    = var.project
+  location                   = var.region
+  delete_contents_on_destroy = true
+}
