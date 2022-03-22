b4fd83fafef24f2a530f78126985913f71466889
ankurchavda <aachavda0789@gmail.com>
2022-03-22 20:09:51 +0000

    events dag template

---
+++ airflow/dags/bigquery_tasks.py
@@ -0,0 +1,129 @@
+from airflow.providers.google.cloud.operators.bigquery import (BigQueryCreateExternalTableOperator, 
+                                                               BigQueryCreateEmptyTableOperator, 
+                                                               BigQueryInsertJobOperator,
+                                                               BigQueryDeleteTableOperator)
+
+
+def create_external_table(gcp_project_id, 
+                          bigquery_dataset, 
+                          external_table_name, 
+                          gcp_gcs_bucket, 
+                          events_path):
+    """
+    Create an external table using the BigQueryCreateExternalTableOperator
+
+    Parameters :
+        gcp_project_id : str
+        bigquery_dataset : str
+        external_table_name : str
+        gcp_gcs_bucket : str
+        events_path : str
+    
+    Returns :
+        task
+    """
+    task = BigQueryCreateExternalTableOperator(
+        task_id = 'create_external_table',
+        table_resource = {
+            'tableReference': {
+            'projectId': gcp_project_id,
+            'datasetId': bigquery_dataset,
+            'tableId': f'{external_table_name}',
+            },
+            'externalDataConfiguration': {
+                'sourceFormat': 'PARQUET',
+                'sourceUris': [f'gs://{gcp_gcs_bucket}/{events_path}/*'],
+            },
+        }
+    )
+
+    return task
+
+
+def create_empty_table(gcp_project_id,
+                       bigquery_dataset,
+                       bigquery_table_name,
+                       events_schema):
+    """
+    Create an empty table in Bigquery using BigQueryCreateEmptyTableOperator
+
+    Parameters :
+        gcp_project_id : str
+        bigquery_dataset : str
+        bigquery_table_name : str
+        events_schema : str
+    
+    Returns :
+        task
+    """
+    task = BigQueryCreateEmptyTableOperator(
+        task_id = 'create_empty_table',
+        project_id = gcp_project_id,
+        dataset_id = bigquery_dataset,
+        table_id = bigquery_table_name,
+        schema_fields = events_schema,
+        time_partitioning = {
+            'type': 'HOUR',
+            'field': 'ts'
+            },
+        exists_ok = True
+    )
+
+    return task
+
+    
+def insert_job(insert_query, 
+               bigquery_dataset, 
+               gcp_project_id,
+               timeout=300000):
+    """
+    Run the insert query using BigQueryInsertJobOperator
+
+    Parameters :
+        insert_query : str
+        bigquery_dataset : str
+        gcp_project_id : str
+
+    Returns : 
+        task
+    """
+    task = BigQueryInsertJobOperator(
+        task_id = 'execute_insert_query',
+        configuration = {
+            'query': {
+                'query': insert_query,
+                'useLegacySql': False
+            },
+            'timeoutMs' : timeout,
+            'defaultDataset' : {
+                'datasetId': bigquery_dataset,
+                'projectId': gcp_project_id
+                }
+            }
+    )
+
+    return task
+
+
+def delete_external_table(gcp_project_id, 
+                          bigquery_dataset, 
+                          external_table_name):
+
+    """
+    Delete table from Big Query using BigQueryDeleteTableOperator
+    Parameters:
+        gcp_project_id : str
+        bigquery_dataset : str
+        external_table_name : str
+
+    Returns:
+        task
+    """
+    
+    task = BigQueryDeleteTableOperator(
+        task_id = 'delete_external_table',
+        deletion_dataset_table = f'{gcp_project_id}.{bigquery_dataset}.{external_table_name}',
+        ignore_if_missing = True
+    )
+
+    return task
---
+++ airflow/dags/schema.py
@@ -0,0 +1,24 @@
+schema = {
+    "listen_events" : [
+        {"name": "artist", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "song", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "duration", "type": "NUMERIC", "mode": "NULLABLE"},
+        {"name": "ts", "type": "TIMESTAMP", "mode": "NULLABLE"},
+        {"name": "sessionid", "type": "INTEGER", "mode": "NULLABLE"},
+        {"name": "auth", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "level", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "itemInSession", "type": "INTEGER", "mode": "NULLABLE"},
+        {"name": "city", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "zip", "type": "INTEGER", "mode": "NULLABLE"},
+        {"name": "state", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "userAgent", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "lon", "type": "NUMERIC", "mode": "NULLABLE"},
+        {"name": "lat", "type": "NUMERIC", "mode": "NULLABLE"},
+        {"name": "userId", "type": "INTEGER", "mode": "NULLABLE"},
+        {"name": "lastName", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "firstName", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "gender", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "registration", "type": "INTEGER", "mode": "NULLABLE"},
+        {"name": "year", "type": "INTEGER", "mode": "NULLABLE"}
+    ]
+}
