80dd750728f7debb95dbaf398658f1511a66e217
ankurchavda <aachavda0789@gmail.com>
2022-03-24 16:27:57 +0000

    dag added

---
+++ airflow/dags/streamify_dag.py
@@ -0,0 +1,107 @@
+import os
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+from schema import schema
+from task_templates import (create_external_table, 
+                            create_empty_table, 
+                            insert_job, 
+                            delete_external_table)
+
+
+EVENTS = ['listen_events', 'page_view_events', 'auth_events'] # we have data coming in from three events
+
+#Make Dynamic
+# BIG_QUERY_TABLE_NAME = 'listen_events'
+# INSERT_QUERY = "{% include 'sql/listen_events.sql' %}"
+# EXTERNAL_TABLE_NAME = f'{BIG_QUERY_TABLE_NAME}_{EXECUTION_DATETIME_STR}'
+# EVENTS_PATH = f'{BIG_QUERY_TABLE_NAME}/month={EXECUTION_MONTH}/day={EXECUTION_DAY}/hour={EXECUTION_HOUR}'
+# EVENTS_SCHEMA = schema[BIG_QUERY_TABLE_NAME]
+###
+
+GCP_PROJECT_ID = os.environ.get('GCP_PROJECT_ID')
+GCP_GCS_BUCKET = os.environ.get('GCP_GCS_BUCKET')
+BIGQUERY_DATASET = os.environ.get('BIGQUERY_DATASET', 'streamify_stg')
+
+EXECUTION_MONTH = '{{ logical_date.strftime("%-m") }}'
+EXECUTION_DAY = '{{ logical_date.strftime("%-d") }}'
+EXECUTION_HOUR = '{{ logical_date.strftime("%-H") }}'
+EXECUTION_DATETIME_STR = '{{ logical_date.strftime("%m%d%H") }}'
+
+TABLE_MAP = { f"{event.upper()}_TABLE" : event for event in EVENTS}
+
+MACRO_VARS = {"GCP_PROJECT_ID":GCP_PROJECT_ID, 
+              "BIGQUERY_DATASET": BIGQUERY_DATASET, 
+              "EXECUTION_DATETIME_STR": EXECUTION_DATETIME_STR
+              }
+
+MACRO_VARS.update(TABLE_MAP)
+
+default_args = {
+    'owner' : 'airflow'
+}
+
+with DAG(
+    dag_id = f'streamify_dag',
+    default_args = default_args,
+    description = f'Hourly data pipeline to generate dims and facts for streamify',
+    schedule_interval="5 * * * *", #At the 5th minute of every hour
+    start_date=datetime(2022,3,20),
+    end_date=datetime(2022,3,20,3),
+    catchup=True,
+    max_active_runs=3,
+    user_defined_macros=MACRO_VARS,
+    tags=['streamify']
+) as dag:
+    
+    initate_dbt_task = BashOperator(
+        task_id = 'dbt_deps_and_seed',
+        bash_command = 'cd /dbt && dbt deps && dbt seed'
+    )
+
+    execute_dbt_task = BashOperator(
+        task_id = 'dbt_streamify_run',
+        bash_command = 'cd /dbt && dbt run'
+    )
+
+    for event in EVENTS:
+        
+        staging_table_name = event
+        insert_query = f"{{% include 'sql/{event}.sql' %}}" #extra {} for f-strings escape
+        external_table_name = f'{staging_table_name}_{EXECUTION_DATETIME_STR}'
+        events_data_path = f'{staging_table_name}/month={EXECUTION_MONTH}/day={EXECUTION_DAY}/hour={EXECUTION_HOUR}'
+        events_schema = schema[event]
+
+        create_external_table_task = create_external_table(event,
+                                                           GCP_PROJECT_ID, 
+                                                           BIGQUERY_DATASET, 
+                                                           external_table_name, 
+                                                           GCP_GCS_BUCKET, 
+                                                           events_data_path)
+
+        create_empty_table_task = create_empty_table(event,
+                                                     GCP_PROJECT_ID,
+                                                     BIGQUERY_DATASET,
+                                                     staging_table_name,
+                                                     events_schema)
+                                                
+        execute_insert_query_task = insert_job(event,
+                                               insert_query,
+                                               BIGQUERY_DATASET,
+                                               GCP_PROJECT_ID)
+
+        delete_external_table_task = delete_external_table(event,
+                                                           GCP_PROJECT_ID, 
+                                                           BIGQUERY_DATASET, 
+                                                           external_table_name)
+                    
+        
+        create_external_table_task >> \
+        create_empty_table_task >> \
+        execute_insert_query_task >> \
+        delete_external_table_task >> \
+        initate_dbt_task >> \
+        execute_dbt_task
+        
--- airflow/dags/task_templates.py
+++ airflow/dags/task_templates.py
@@ -4,7 +4,8 @@ from airflow.providers.google.cloud.operators.bigquery import (BigQueryCreateExt
                                                                BigQueryDeleteTableOperator)
 
 
-def create_external_table(gcp_project_id, 
+def create_external_table(event,
+                          gcp_project_id, 
                           bigquery_dataset, 
                           external_table_name, 
                           gcp_gcs_bucket, 
@@ -23,7 +24,7 @@ def create_external_table(gcp_project_id,
         task
     """
     task = BigQueryCreateExternalTableOperator(
-        task_id = 'create_external_table',
+        task_id = f'{event}_create_external_table',
         table_resource = {
             'tableReference': {
             'projectId': gcp_project_id,
@@ -40,7 +41,8 @@ def create_external_table(gcp_project_id,
     return task
 
 
-def create_empty_table(gcp_project_id,
+def create_empty_table(event,
+                       gcp_project_id,
                        bigquery_dataset,
                        bigquery_table_name,
                        events_schema):
@@ -57,7 +59,7 @@ def create_empty_table(gcp_project_id,
         task
     """
     task = BigQueryCreateEmptyTableOperator(
-        task_id = 'create_empty_table',
+        task_id = f'{event}_create_empty_table',
         project_id = gcp_project_id,
         dataset_id = bigquery_dataset,
         table_id = bigquery_table_name,
@@ -72,7 +74,8 @@ def create_empty_table(gcp_project_id,
     return task
 
     
-def insert_job(insert_query, 
+def insert_job(event,
+               insert_query_location,
                bigquery_dataset, 
                gcp_project_id,
                timeout=300000):
@@ -87,11 +90,12 @@ def insert_job(insert_query,
     Returns : 
         task
     """
+
     task = BigQueryInsertJobOperator(
-        task_id = 'execute_insert_query',
+        task_id = f'{event}_execute_insert_query',
         configuration = {
             'query': {
-                'query': insert_query,
+                'query': insert_query_location,
                 'useLegacySql': False
             },
             'timeoutMs' : timeout,
@@ -105,7 +109,8 @@ def insert_job(insert_query,
     return task
 
 
-def delete_external_table(gcp_project_id, 
+def delete_external_table(event,
+                          gcp_project_id, 
                           bigquery_dataset, 
                           external_table_name):
 
@@ -121,7 +126,7 @@ def delete_external_table(gcp_project_id,
     """
     
     task = BigQueryDeleteTableOperator(
-        task_id = 'delete_external_table',
+        task_id = f'{event}_delete_external_table',
         deletion_dataset_table = f'{gcp_project_id}.{bigquery_dataset}.{external_table_name}',
         ignore_if_missing = True
     )
