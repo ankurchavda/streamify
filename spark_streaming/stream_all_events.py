554a4112b7c27a278b735214ab7622bcd0885771
tremoloplusdelay <aachavda0789@gmail.com>
2022-03-26 00:13:01 +0530

    double type, string encoding

--- spark_streaming/stream_all_events.py
+++ spark_streaming/stream_all_events.py
@@ -5,16 +5,20 @@
 
 import os
 from streaming_functions import *
-from schema import listen_events_schema, page_view_events_schema, auth_events_schema
+from schema import schema
 
-KAFKA_ADDRESS = os.getenv("KAFKA_ADDRESS", 'localhost')
-KAFKA_PORT = "9092"
-# will raise error if env variable not found
-GCS_STORAGE_PATH = os.environ["GCS_STORAGE_PATH"]
+# Kafka Topics
 LISTEN_EVENTS_TOPIC = "listen_events"
 PAGE_VIEW_EVENTS_TOPIC = "page_view_events"
 AUTH_EVENTS_TOPIC = "auth_events"
+
+KAFKA_PORT = "9092"
+
 AWAIT_TERMINATION_TIMEOUT = 60
+
+KAFKA_ADDRESS = os.getenv("KAFKA_ADDRESS", 'localhost')
+# will raise error if env variable not found
+GCS_STORAGE_PATH = os.environ["GCS_STORAGE_PATH"]
 
 # initialize a spark session
 spark = create_or_get_spark_session('Eventsim Stream')
@@ -22,17 +26,20 @@ spark.streams.resetTerminated()
 # listen events stream
 listen_events = create_kafka_read_stream(
     spark, KAFKA_ADDRESS, KAFKA_PORT, LISTEN_EVENTS_TOPIC)
-listen_events = process_stream(listen_events, listen_events_schema)
+listen_events = process_stream(
+    listen_events, schema[LISTEN_EVENTS_TOPIC], LISTEN_EVENTS_TOPIC)
 
 # page view stream
 page_view_events = create_kafka_read_stream(
     spark, KAFKA_ADDRESS, KAFKA_PORT, PAGE_VIEW_EVENTS_TOPIC)
-page_view_events = process_stream(page_view_events, page_view_events_schema)
+page_view_events = process_stream(
+    page_view_events, schema[PAGE_VIEW_EVENTS_TOPIC], PAGE_VIEW_EVENTS_TOPIC)
 
 # auth stream
 auth_events = create_kafka_read_stream(
     spark, KAFKA_ADDRESS, KAFKA_PORT, AUTH_EVENTS_TOPIC)
-auth_events = process_stream(auth_events, auth_events_schema)
+auth_events = process_stream(
+    auth_events, schema[AUTH_EVENTS_TOPIC], AUTH_EVENTS_TOPIC)
 
 # write a file to storage every 2 minutes in parquet format
 listen_events_writer = create_file_write_stream(listen_events,
--- spark_streaming/streaming_functions.py
+++ spark_streaming/streaming_functions.py
@@ -1,6 +1,18 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col, month, hour, dayofmonth, col, year
+from pyspark.sql.functions import from_json, col, month, hour, dayofmonth, col, year, udf
 
+
+@udf
+def string_decode(s, encoding='utf-8'):
+    if s:
+        return (s.encode('latin1')         # To bytes, required by 'unicode-escape'
+                .decode('unicode-escape') # Perform the actual octal-escaping decode
+                .encode('latin1')         # 1:1 mapping back to bytes
+                .decode(encoding)         # Decode original encoding
+                .strip('\"'))
+
+    else:
+        return s
 
 def create_or_get_spark_session(app_name, master="yarn"):
     """
@@ -52,7 +64,7 @@ def create_kafka_read_stream(spark, kafka_address, kafka_port, topic, starting_o
     return read_stream
 
 
-def process_stream(stream, stream_schema):
+def process_stream(stream, stream_schema, topic):
     """
     Process stream to fetch on value from the kafka message.
     convert ts to timestamp format and produce year, month, day,
@@ -83,6 +95,14 @@ def process_stream(stream, stream_schema):
               .withColumn("hour", hour(col("ts")))
               .withColumn("day", dayofmonth(col("ts")))
               )
+
+    # rectify string encoding
+    if topic in ["listen_events", "page_view_events"]:
+        stream = (stream
+                .withColumn("song", string_decode("song"))
+                .withColumn("artist", string_decode("artist")) 
+                )
+
 
     return stream
 
